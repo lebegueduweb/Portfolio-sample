9573e0ebd39e61b199c72a9c4523778db779be18
lebegueduweb <patvokouma@gmail.com>
2018-08-16 00:30:11 +0300

    added somefiles and changes for deployment requirements to heroku

--- skeleton_recovery/settings.py
+++ skeleton_recovery/settings.py
@@ -157,3 +157,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
